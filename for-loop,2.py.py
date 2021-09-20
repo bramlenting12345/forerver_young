c556cf6fafd024f8f6ef743d650353b8f74be95a
bramlenting1234 <99068035@mydavinci.nl>
2021-09-20 10:29:04 +0200

    alles

---
+++ for-loop,2.py.py
@@ -0,0 +1,4 @@
+for y in range (30, 0, -1):
+    print(y)
+
+print("raketlancering")
---
+++ for-loop-3.py.py
@@ -0,0 +1,10 @@
+for y in range (1, 13):
+    print(y)
+    print("am")
+
+for i in range (12, 25):
+    print(i)
+    print("pm")
+
+
+    
